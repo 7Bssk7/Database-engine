a9bcdc9b021435e27c63b3bb0628d4929795d5a3
7Bssk7 <arhip.fin3@gmail.com>
2026-09-07 20:16:50 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include <cstdint>
 
 using namespace std;
+
+unordered_map<string, uint64_t> hash_table;
 
 void write_record(const string& filename, const string& key, const string& value){
     ofstream file(filename, ios::binary || ios::app);
@@ -17,10 +20,16 @@ void write_record(const string& filename, const string& key, const string& value
     uint32_t key_len = static_cast<uint32_t>(key.size());
     uint32_t val_len = static_cast<uint32_t>(value.size());
 
+    file.write()
+
 
 }
 
 int main(){
+
+    string file = "db.bin";
+
+    write_record(file, "user-1", "Bssk");
 
     
 
