2db33a9492dc0f4d95af3eeb437e814f96e3b99d
7Bssk7 <arhip.fin3@gmail.com>
2026-09-07 22:01:53 -0700

    final commit

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <unordered_map>
 #include <cstdint>
+#include <iomanip>
 
 using namespace std;
 
 unordered_map<string, uint64_t> hash_table;
 
-void write_record(const string& filename, const string& key, const string& value){
+void write_record(const string& filename, const string& key, const string& value, const int& count){
     ofstream file(filename, ios::binary | ios::app);
     if(!file){
         cout << "Failed to open the file!" << endl;
@@ -17,7 +18,13 @@ void write_record(const string& filename, const string& key, const string& value
 
     }
 
+
+    if(count > 1){
+        file.seekp(0, ios::end);
+    }
+
     uint64_t offset = static_cast<uint64_t>(file.tellp());
+
     uint32_t key_len = static_cast<uint32_t>(key.size());
     uint32_t val_len = static_cast<uint32_t>(value.size());
 
@@ -27,11 +34,11 @@ void write_record(const string& filename, const string& key, const string& value
     file.write(key.data(), key_len);
     file.write(value.data(), val_len);
 
-    hash_table[key] = offset;
-
     file.close();
 
-    cout << "Saved: '" << key  <<"' with value of '" << value << "'at byte offset " << offset << endl;
+    hash_table[key] = offset;
+
+    cout << "Saved: '" << key  <<"' with value of '" << value << "' at byte offset " << offset << endl;
 
 
 }
@@ -39,7 +46,7 @@ void write_record(const string& filename, const string& key, const string& value
 string read_record(const string& filename, const string& key){
    
     if(hash_table.find(key) == hash_table.end()){
-        cout << "Error Key: '" << key << "' was not found" << endl;
+        cout << " Error Key: '" << key << "' was not found" << endl;
 
         return "";
     }
@@ -76,8 +83,69 @@ string read_record(const string& filename, const string& key){
 int main(){
 
     string file = "db.bin";
+    bool check = true;
 
-    write_record(file, "user-1", "Bssk");
+    
+    string userName;
+    string userMessage; 
+    int counter = 0;
+    char ch;
+
+    counter += 1;
+    write_record(file, "user-1", "Bssk", counter);
+
+    do{
+        cout << "Enter your user name: ";
+        getline(cin, userName);
+        cout << endl;
+
+        cout << "Enter your message: ";
+        getline(cin, userMessage);
+        cout << endl;
+
+        counter += 1;
+
+        write_record(file, userName, userMessage, counter);
+
+        
+        cout << "Press any key to continue, otherwise press 'n'." << endl;
+        cin >> ch;
+        cin.ignore();
+
+        if(ch == 'n'){
+            check = false;
+        }
+
+
+    }while(check != false);
+
+
+    cout << endl;
+    cout << endl;
+    
+    check = true;
+    cout << "------ Look up data from data base ------" << endl;
+    cout << "Example: User-1: " << read_record(file, "user-1") << endl;
+
+    do{
+        cout << "Enter your user name: ";
+        getline(cin, userName);
+
+        cout << userName << ": " << read_record(file, userName) << endl;
+
+        cout << "Press any key to continue, otherwise press 'n'." << endl;
+        cin >> ch;
+        cin.ignore();
+
+        if(ch == 'n'){
+            check = false;
+        }
+
+    }while(check != false);
+
+    cout << "Number of elemnts added in this session: " << counter << endl;
+
+
 
     
 
