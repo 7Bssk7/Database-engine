6885208e9ecab06d1a4af8459e1ab3caa6510f96
7Bssk7 <arhip.fin3@gmail.com>
2026-09-07 20:25:26 -0700

    third commit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 unordered_map<string, uint64_t> hash_table;
 
 void write_record(const string& filename, const string& key, const string& value){
-    ofstream file(filename, ios::binary || ios::app);
+    ofstream file(filename, ios::binary | ios::app);
     if(!file.is_open()){
         cout << "Failed to open the file!" << endl;
 
@@ -17,10 +17,19 @@ void write_record(const string& filename, const string& key, const string& value
 
     }
 
+    uint64_t offset = static_cast<uint64_t>(file.tellp());
     uint32_t key_len = static_cast<uint32_t>(key.size());
     uint32_t val_len = static_cast<uint32_t>(value.size());
 
-    file.write()
+    file.write(reinterpret_cast<const char*>(&key_len), sizeof(key_len));
+    file.write(reinterpret_cast<const char*>(&val_len), sizeof(val_len));
+
+    file.write(key.data(), key_len);
+    file.write(value.data(), val_len);
+
+    hash_table[key] = offset;
+
+    cout << "Saved: '" << key  <<"' with value of '" << value << "'at byte offset " << offset << endl;
 
 
 }
